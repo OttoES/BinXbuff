91ec6af2e3a71f19e9a301cd22d648b68debbff5
otto <ottox123@gmail.com>
2018-09-10 07:38:45 +0200

    Busy wih basic parsing changes

--- src/bingen.py
+++ src/bingen.py
@@ -7,75 +7,124 @@
 #
 
 from pyparsing import (Word, alphas, alphanums, Regex, Suppress, Forward,
-    Group, oneOf, ZeroOrMore, Optional, delimitedList, Keyword,
+    Group, oneOf, ZeroOrMore, Optional, delimitedList, Keyword, cStyleComment, pythonStyleComment,
     restOfLine, quotedString, Dict)
+
+comment       = '## ' + restOfLine
+#comment       = '#' + restOfLine
+CMNT          = cStyleComment("comment")
+#CMNT2         = cppStyleComment("comment")
 
 ident = Word(alphas+"_",alphanums+"_").setName("identifier")
 integer = Regex(r"[+-]?\d+")
 
 LBRACE,RBRACE,LBRACK,RBRACK,LPAR,RPAR,EQ,SEMI = map(Suppress,"{}[]()=;")
 
-kwds = """message required optional repeated enum extensions extends extend 
-          to package service rpc returns true false option import"""
+#kwds = """message required optional repeated enum extensions extends extend 
+#          to package service rpc returns true false option import"""
+
+kwds = """struct enum extensions extends extend 
+          required optional array
+          true false option import"""
+
 for kw in kwds.split():
     exec("{}_ = Keyword('{}')".format(kw.upper(), kw))
 
-messageBody = Forward()
+structtBody         = Forward()
 
-messageDefn = MESSAGE_ - ident("messageId") + LBRACE + messageBody("body") + RBRACE
+structDefn          = Optional(CMNT("comment")) + STRUCT_ - ident("structName") + LBRACE + structtBody("body") + RBRACE
 
-typespec = oneOf("""double float int32 int64 uint32 uint64 sint32 sint64 
-                    fixed32 fixed64 sfixed32 sfixed64 bool string bytes""") | ident
-rvalue = integer | TRUE_ | FALSE_ | ident
-fieldDirective = LBRACK + Group(ident + EQ + rvalue) + RBRACK
-fieldDefn = (( REQUIRED_ | OPTIONAL_ | REPEATED_ )("fieldQualifier") - 
-              typespec("typespec") + ident("ident") + EQ + integer("fieldint") + ZeroOrMore(fieldDirective) + SEMI)
+#typespec = oneOf("""double float int32 int64 uint32 uint64 sint32 sint64 
+#                    fixed32 fixed64 sfixed32 sfixed64 bool string bytes""") | ident
+typespec            = oneOf("""int8 uint8 int16 uint16 int32 int64 uint32 uint64 bool char string zstring bytes enum8 enum16 enum32 TAG8 TAG16 """) | ident
+rvalue              = integer | TRUE_ | FALSE_ | ident
+fieldDirective      = LBRACK + Group(ident + EQ + rvalue) + RBRACK
+fieldDefn           = (( REQUIRED_ | OPTIONAL_ | ARRAY_ )("fieldQualifier") - 
+                      typespec("typespec") + ident("ident") + EQ + integer("fieldint") + ZeroOrMore(fieldDirective) + SEMI)
 
-# enumDefn ::= 'enum' ident '{' { ident '=' integer ';' }* '}'
-enumDefn = ENUM_("typespec") - ident('name') + LBRACE + Dict( ZeroOrMore( Group(ident + EQ + integer + SEMI) ))('values') + RBRACE
+# enumDefn        ::= 'enum' ident '{' { ident '=' integer ';' }* '}'
+enumDefn            = Optional(CMNT("comment")) + ENUM_("typespec") - ident('name') + LBRACE + Dict( ZeroOrMore( Group(ident + EQ + integer + SEMI) ))('values') + RBRACE
 
 # extensionsDefn ::= 'extensions' integer 'to' integer ';'
-extensionsDefn = EXTENSIONS_ - integer + TO_ + integer + SEMI
+##extensionsDefn = EXTENSIONS_ - integer + TO_ + integer + SEMI
 
-# messageExtension ::= 'extend' ident '{' messageBody '}'
-messageExtension = EXTEND_ - ident + LBRACE + messageBody + RBRACE
+# structExtension ::= 'extend' ident '{' messageBody '}'
+structExtension     = Optional(CMNT("comment")) + EXTEND_ - ident + LBRACE + structtBody + RBRACE
 
-# messageBody ::= { fieldDefn | enumDefn | messageDefn | extensionsDefn | messageExtension }*
-messageBody << Group(ZeroOrMore( Group(fieldDefn | enumDefn | messageDefn | extensionsDefn | messageExtension) ))
+# messageBody     ::= { fieldDefn | enumDefn | structDefn | extensionsDefn | structExtension }*
+##messageBody << Group(ZeroOrMore( Group(fieldDefn | enumDefn | structDefn | extensionsDefn | structExtension) ))
+structtBody << Group(ZeroOrMore( Group(fieldDefn | enumDefn | structDefn | structExtension) ))
 
 # methodDefn ::= 'rpc' ident '(' [ ident ] ')' 'returns' '(' [ ident ] ')' ';'
-methodDefn = (RPC_ - ident("methodName") + 
-              LPAR + Optional(ident("methodParam")) + RPAR + 
-              RETURNS_ + LPAR + Optional(ident("methodReturn")) + RPAR)
+# methodDefn = (RPC_ - ident("methodName") + 
+#               LPAR + Optional(ident("methodParam")) + RPAR + 
+#               RETURNS_ + LPAR + Optional(ident("methodReturn")) + RPAR)
 
 # serviceDefn ::= 'service' ident '{' methodDefn* '}'
-serviceDefn = SERVICE_ - ident("serviceName") + LBRACE + ZeroOrMore(Group(methodDefn)) + RBRACE
+##serviceDefn = SERVICE_ - ident("serviceName") + LBRACE + ZeroOrMore(Group(methodDefn)) + RBRACE
 
 # packageDirective ::= 'package' ident [ '.' ident]* ';'
-packageDirective = Group(PACKAGE_ - delimitedList(ident, '.', combine=True) + SEMI)
+##packageDirective = Group(PACKAGE_ - delimitedList(ident, '.', combine=True) + SEMI)
 
-comment = '//' + restOfLine
+
 
 importDirective = IMPORT_ - quotedString("importFileSpec") + SEMI
 
 optionDirective = OPTION_ - ident("optionName") + EQ + quotedString("optionValue") + SEMI
 
-topLevelStatement = Group(messageDefn | messageExtension | enumDefn | serviceDefn | importDirective | optionDirective)
+#topLevelStatement = Group(structDefn | structExtension | enumDefn | serviceDefn | importDirective | optionDirective)
+topLevelStatement = Group(structDefn | structExtension | enumDefn | importDirective | optionDirective)
 
-parser = Optional(packageDirective) + ZeroOrMore(topLevelStatement)
+##parser = Optional(packageDirective) + ZeroOrMore(topLevelStatement)
+parser =  ZeroOrMore(topLevelStatement)
 
 parser.ignore(comment)
 
 
-test1 = """message Person { 
+test1 = """struct Person { 
   required int32 id = 1; 
   required string name = 2; 
   optional string email = 3; 
 }"""
 
-test2 = """package tutorial;
+test2 = """
 
-message Person {
+  enum Gender {
+    UNKNOWN = 0;
+    MALE = 1;
+    FEMLE = 2;
+  }
+## package tutorial;
+
+struct Person {
+  required string name = 1;
+  required int32 id = 2;
+  optional string email = 3;
+  required Gender gen = 8;
+  /* enum comment */
+  enum PhoneType {
+    MOBILE = 0;
+    HOME = 1;
+    WORK = 2;
+  }
+
+  struct PhoneNumber {
+    required string number = 1;
+    optional PhoneType type = 2; ## [default = HOME];
+  }
+
+  required PhoneNumber phone = 4;
+}
+/* 
+ My comment
+*/
+struct AddressBook {
+  required Person person = 1;
+}"""
+
+test3 = """//package tutorial;
+
+struct Person {
   required string name = 1;
   required int32 id = 2;
   optional string email = 3;
@@ -86,16 +135,11 @@ message Person {
     WORK = 2;
   }
 
-  message PhoneNumber {
-    required string number = 1;
-    optional PhoneType type = 2 [default = HOME];
-  }
 
-  repeated PhoneNumber phone = 4;
 }
 
-message AddressBook {
-  repeated Person person = 1;
+struct AddressBook {
+  required int16 person = 1;
 }"""
 
 parser.runTests([test1, test2])
